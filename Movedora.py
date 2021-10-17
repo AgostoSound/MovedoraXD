3047f701a8fbeab4cef9207003e116082a3ea848
AgostoSound <agostosound@gmail.com>
2021-10-17 19:45:27 -0300

    Print carpetas

--- Movedora.py
+++ Movedora.py
@@ -2,12 +2,17 @@
 import os
 
 #______________ HARD CODE _________________________________
-dir_input = 'C:\Users\Agosto\Desktop\Patin2021\Disney'
+dir_input = 'C:/Users/Agosto/Desktop/Patin2021/Disney'
 vec_direct = os.listdir(dir_input)
-dir_output = 'C:\Users\Agosto\Desktop\Patin2021'
-
+dir_output = 'C:/Users/Agosto/Desktop/Patin2021'
 
 #______________ FUNCIONES _________________________________
 
 #______________ PRINCIPAL _________________________________
+def main():
+    for carpeta in vec_direct:
+        print(carpeta)
+
+
+main()
 
